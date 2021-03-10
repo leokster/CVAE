ca7b7d7eab9ec0c2405635bef11c92ac6546fa4a
Koen <koen.vanwalstijn@hevs.ch>
2021-03-10 10:42:32 +0100

    Gaussian likelihood loss with normalization

--- variational_autoencoder/losses.py
+++ variational_autoencoder/losses.py
@@ -1,15 +1,20 @@
 from tensorflow.keras.losses import Loss
 import tensorflow as tf
+import math
 
-def _get_slice_last_dim(ten, idx):
-    dims = len(ten.shape)
-    return tf.slice(ten, [0]*(dims-1)+[idx], [-1]*(dims-1)+[1])
-
-class FullLikelihood(Loss):
+class GaussianLikelihood(Loss):
+    def __init__(self, normalize=True, **kwargs):
+        super(GaussianLikelihood, self).__init__(**kwargs)
+        self.normalize = normalize
+    
     def call(self, y_true, y_pred):
-        mean = _get_slice_last_dim(y_pred,0)
-        sd = _get_slice_last_dim(y_pred,1)
-        reconstruction_loss = tf.reduce_mean(
-            0.5 * tf.square(y_true - mean) * tf.exp(-sd) + sd
-        )
-        return reconstruction_loss
+        mean = y_pred[...,0]
+        log_var = y_pred[...,1]
+        loss =  (tf.square(y_true - mean)/tf.exp(log_var) 
+                 + log_var + math.log(2*math.pi))/2
+        loss = tf.reduce_mean(tf.reduce_sum(loss, axis=-1))
+        if self.normalize==True:
+            loss /= mean.shape[-1]
+        if isinstance(self.normalize, (int, float)):
+            loss /= self.normalize
+        return loss
